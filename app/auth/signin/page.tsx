790e2424b15bf1ba7155945130569df2dac8cbd3
ryuta <aresvenus121039@gmail.com>
2024-03-08 05:41:52 +0900

    vercel deployment build fixed.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -58,7 +58,7 @@ export default function SignIn() {
                 </button>
 
                 <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
-                  Don't have an account?
+                  Don&apos;t have an account?
                   <a
                     href="#!"
                     className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
 "use client"
-import { useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
-import axios from 'axios';
 
 export default function Home() {
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/api/auth')
-    },
-  })
-  useEffect(() => {
-      const fetch =async () => {
-        //test for integrating prisma and supabase
-        const users = await axios.get('/api/users');
-      }
-      fetch()
-  }, [])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       First Page
