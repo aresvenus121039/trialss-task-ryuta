c1494f1f3d6b722c51487f3a7a8d9248769d8d7b
ryuta <aresvenus121039@gmail.com>
2024-03-08 05:28:40 +0900

    Created Login Page.

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+
+export default function SignIn() {
+  return (
+    <section className="h-screen px-7">
+      <div className="h-full">
+        <div
+          className="flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div
+            className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+            <img
+              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="w-full"
+              
+              alt="Sample image" />
+          </div>
+
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+            <form>
+              <div className="relative mb-6" data-twe-input-wrapper-init>
+                <Input
+                  type="email"
+                  placeholder="Email address" />
+              </div>
+
+              <div className="relative mb-6" data-twe-input-wrapper-init>
+                <Input
+                  type="password"
+                  placeholder="Password" />
+              </div>
+
+              <div className="mb-6 flex items-center justify-between">
+                <div className="mb-[0.125rem] block min-h-[1.5rem] ps-[1.5rem]">
+                  <input
+                    className="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary"
+                    type="checkbox"
+                    value=""
+                    id="exampleCheck2" />
+                  <label
+                    className="inline-block ps-[0.15rem] hover:cursor-pointer"
+                    htmlFor="exampleCheck2">
+                    Remember me
+                  </label>
+                </div>
+
+                <a href="#!">Forgot password?</a>
+              </div>
+
+              <div className="text-center lg:text-left">
+                <button
+                  type="button"
+                  className="inline-block w-full rounded bg-primary px-7 pb-2 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                  data-twe-ripple-init
+                  data-twe-ripple-color="light">
+                  Login
+                </button>
+
+                <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
+                  Don't have an account?
+                  <a
+                    href="#!"
+                    className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
+                    >Register</a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
 import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 import axios from 'axios';
 
 export default function Home() {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/api/auth')
+    },
+  })
   useEffect(() => {
       const fetch =async () => {
         //test for integrating prisma and supabase
